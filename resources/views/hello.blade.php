9dc2684c297d90213b44c6a92fe96fc76204d202
ferdyvalentino123 <ferdyvalentino13@student.ittelkom-sby.ac.id>
2024-05-27 20:57:43 +0700

    halaman hello

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Telkom Petshop</title>
+</head>
+<body>
+    <H1>Get Royal Canin For your cat!</H1>
+</body>
+</html>
